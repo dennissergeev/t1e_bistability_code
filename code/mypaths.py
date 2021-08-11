b5e1cda2bc6421ffd9f0a4da65209abd0f0cdc5e
Dennis <dennis.sergeev@gmail.com>
2021-08-11 20:55:33 +0100

    add the file with paths

---
+++ code/mypaths.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Paths to data."""
+from pathlib import Path
+
+# Top-level directory containing code and data (one level up)
+topdir = Path(__file__).absolute().parent.parent
+
+# Modelling results
+datadir = topdir.parent / "modelling" / "um" / "results"
+
+sadir = datadir / "sa"  # standalone suites directory
+
+# Plotting output
+plotdir = topdir / "plots"
+plotdir.mkdir(parents=True, exist_ok=True)
+
+# TeX output (tables)
+tabdir = topdir / "tables"
+tabdir.mkdir(parents=True, exist_ok=True)
